1485f1a36c7ed0bc377652641e0655fa9df76b60
RishiChalla <rishik.challa@gmail.com>
2023-11-11 23:24:04 -0500

    Quick code cleanup and dark mode

--- js/cell.js
+++ js/cell.js
@@ -9,53 +9,22 @@ var Cell;
 	};
 
 	Cell.prototype.draw = function() {
-		if (this.value == 0) {
-			ctx.fillStyle = "gray";
-		}
-		if (this.value == 2) {
-			ctx.fillStyle = "#cccc00";
-		}
-		if (this.value == 4) {
-			ctx.fillStyle = "#999900";
-		}
-		if (this.value == 8) {
-			ctx.fillStyle = "#ffa500";
-		}
-		if (this.value == 16) {
-			ctx.fillStyle = "#ff0000";
-		}
-		if (this.value == 32) {
-			ctx.fillStyle = "#66ccff";
-		}
-		if (this.value == 64) {
-			ctx.fillStyle = "#0099e6";
-		}
-		if (this.value == 128) {
-			ctx.fillStyle = "#ff99ff";
-		}
-		if (this.value == 256) {
-			ctx.fillStyle = "#ff1aff";
-		}
-		if (this.value == 512) {
-			ctx.fillStyle = "#b300b3";
-		}
-		if (this.value == 1024) {
-			ctx.fillStyle = "#0000b3";
-		}
-		if (this.value == 2048) {
-			ctx.fillStyle = "#000066";
-		}
-		if (this.value > 2048) {
-			ctx.fillStyle = "purple";
-		}
+		// Set fill style
+		const fillMap = {
+			"0": "gray", "2": "#cccc00", "4": "#999900", "8": "#ffa500", "16": "#ff0000", "32": "#66ccff",
+			"64": "#0099e6", "128": "#ff99ff", "256": "#ff1aff", "512": "#b300b3", "1024": "#0000b3", "2048": "#000066"
+		};
+		if (this.value > 2048) ctx.fillStyle = "purple";
+		else ctx.fillStyle = fillMap[this.value.toString()];
+
+		// Draw
 		ctx.roundRect(this.pos, this.size, 50);
 		ctx.fill();
-		if (this.value != 0) {
-			ctx.font = "100px Comic Sans";
-			ctx.fillStyle = "#ffffff";
-			ctx.textAlign = "center";
-			ctx.fillText(this.value, this.pos.x + this.size.width / 2, this.pos.y + this.size.height / 2 + 30);
-		}
+		if (this.value == 0) return;
+		ctx.font = "100px Arial";
+		ctx.fillStyle = "#ffffff";
+		ctx.textAlign = "center";
+		ctx.fillText(this.value, this.pos.x + this.size.width / 2, this.pos.y + this.size.height / 2 + 30);
 	};
 
 	Cell.prototype.update = function() {
--- js/script.js
+++ js/script.js
@@ -59,7 +59,8 @@ function setup() {
 }
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	board.draw();
 }
 
